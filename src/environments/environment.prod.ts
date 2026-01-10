ce1e347c7a92065176f0ca1d934078d0155275c2
Mohmed Kamal <mohamed.elsharif022@gmail.com>
2026-01-11 01:44:54 +0200

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.gymunity.com/api',
+    apiUrl: 'https://gymunity-fp-apis.runasp.net',
     trainerUrl: 'https://gymunity.netlify.app',
     googleClientId: '' // Replace with actual Google Client ID
 };
